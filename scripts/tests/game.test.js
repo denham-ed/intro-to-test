7300501611be802c3d9ebc919e7088ad9067c554
Ed Denham <ed.denham@gmail.com>
2022-12-31 22:21:19 +0000

    Write first game test

---
+++ scripts/tests/game.test.js
@@ -0,0 +1,21 @@
+/**
+ * @jest-environment jsdom
+ */
+
+beforeAll(() => {
+    let fs = require("fs")
+    let fileContents = fs.readFileSync("index.html","utf-8")
+    document.open()
+    document.write(fileContents)
+    document.close()
+})
+
+let game = {
+    score: 0
+}
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true)
+    })
+})
